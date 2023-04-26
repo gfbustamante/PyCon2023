e7c083b94fe7c1954f4ca69134acf90eedf3f2ad
CadenEbert <116670041+CadenEbert@users.noreply.github.com>
2023-04-26 12:37:24 -0600

    testing switch

--- Circuit_Playground_Bluefruit/Attendee_Examples/caden.py
+++ Circuit_Playground_Bluefruit/Attendee_Examples/caden.py
@@ -3,20 +3,26 @@ import time
 
 
 cp.pixels.brightness = 0.3
+while cp.switch == False:
+    for i in range(9):
+        cp.pixels[i] = (100, 100, 200)
 
-while True:
+while cp.switch == True:
+    
     if cp.button_a:
         cp.pixels[0] = (255, 0, 0)
-        cp.pixels[1] = (255, 0, 0)
+        cp.pixels[1] = (235, 0, 0)
         cp.pixels[2] = (210, 45, 0)
         cp.pixels[3] = (210, 100, 0)
         cp.pixels[4] = (210, 150, 0)
-        # cp.play_file("audio/BassDrum.wav")
+        cp.play_file("audio/BassDrum.wav")
+        time.sleep(0.1)
     if cp.button_b:
-        cp.pixels[5] = (255, 0, 0)
-        cp.pixels[6] = (255, 0, 0)
+        cp.pixels[5] = (210, 150, 0)
+        cp.pixels[6] = (210, 100, 0)
         cp.pixels[7] = (210, 45, 0)
-        cp.pixels[8] = (210, 100, 0)
-        cp.pixels[9] = (210, 150, 0)
+        cp.pixels[8] = (235, 0, 0)
+        cp.pixels[9] = (255, 0, 0)
         cp.play_file("audio/CrashCymbal1.wav")
-    time.sleep(0.1)
+        time.sleep(0.1)
+    
