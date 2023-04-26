cf170097a939acced31917ae4431bf8f4626706c
Greg Bustamante <gfbustamante@gmail.com>
2023-04-26 11:50:15 -0600

    Live edits during testing

--- Circuit_Playground_Bluefruit/Attendee_Examples/caden.py
+++ Circuit_Playground_Bluefruit/Attendee_Examples/caden.py
@@ -6,17 +6,17 @@ cp.pixels.brightness = 0.3
 
 while True:
     if cp.button_a:
-        cp.pixels[5] = (255, 0, 0)
-        cp.pixels[6] = (255, 0, 0)
-        cp.pixels[7] = (210, 45, 0)
-        cp.pixels[8] = (210, 100, 0)
-        cp.pixels[9] = (210, 150, 0)
-        cp.play_file("audio/BassDrum.wav")
-    if cp.button_b:
         cp.pixels[0] = (255, 0, 0)
         cp.pixels[1] = (255, 0, 0)
         cp.pixels[2] = (210, 45, 0)
         cp.pixels[3] = (210, 100, 0)
         cp.pixels[4] = (210, 150, 0)
-        cp.play_file("audio/CrashCymbal.wav")
+        # cp.play_file("audio/BassDrum.wav")
+    if cp.button_b:
+        cp.pixels[5] = (255, 0, 0)
+        cp.pixels[6] = (255, 0, 0)
+        cp.pixels[7] = (210, 45, 0)
+        cp.pixels[8] = (210, 100, 0)
+        cp.pixels[9] = (210, 150, 0)
+        cp.play_file("audio/CrashCymbal1.wav")
     time.sleep(0.1)
